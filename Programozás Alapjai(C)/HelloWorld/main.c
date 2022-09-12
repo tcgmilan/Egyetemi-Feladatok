ebd5bbf77b4773200b15157a0b7b28384de97994
tcgmilan <hbazsi024@gmail.com>
2022-09-12 08:53:41 +0200

    Első órai példafeladat

---
+++ Programozás Alapjai(C)/HelloWorld/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World! \n");
+    printf("It is very early \n");
+
+    printf("#====================#====================#\n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#                    #                    # \n");
+    printf("#====================#====================#\n");
+    return 0;
+}
